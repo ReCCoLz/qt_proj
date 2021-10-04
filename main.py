35f1d2b5d2426e9588c24d899179b980a4938827
ReCColz <recollectionwindscribe@gmail.com>
2021-10-04 14:09:04 +0300

    s

--- main.py
+++ main.py
@@ -5,11 +5,6 @@ key = 'access_key=70402a075db79ce14ef44d254f828ec9'
 
 
 def data(date, url=url, acc=key):
-    req_late = requests.get(f'{url}' + 'latest?' + f'{acc}')
-
-    with open('latests.json', 'w') as file:
-        file.write(req_late.text)
-
     req_his = requests.get(f'{url}' + f'{date}?' + f'{acc}')
 
     with open('historical.json', 'w') as f:
