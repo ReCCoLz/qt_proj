efa2b00f427b8728206a8c2e7473a5b6d8e7bba6
ReCColz <recollectionwindscribe@gmail.com>
2021-10-08 00:03:27 +0300

    asdsad

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import requests
+import json
 
 url = 'http://data.fixer.io/api/'
-key = 'access_key=70402a075db79ce14ef44d254f828ec9'
+key = 'access_key=f83859c0187edb449a865f5e632951aa'
 
 
 def data(date, url=url, acc=key):
     req_his = requests.get(f'{url}' + f'{date}?' + f'{acc}')
+    date = str(date).split('-')
 
-    with open('historical.json', 'w') as f:
+    with open(f'C://Users//recol//Documents//my_projects(python)//qt_proj//data//historical{date[0]}.{date[1]}.json', 'w') as f:
         f.write(req_his.text)
 
 
-data('2000-11-10')
+for year in range(21):
+    for mounth in range(1, 12+1):
+        if year < 10:
+            if mounth < 10:
+                data(f'200{year}-0{mounth}-10')
+            else:
+                data(f'200{year}-{mounth}-10')
+        else:
+            if mounth < 10:
+                data(f'20{year}-0{mounth}-10')
+            else:
+                data(f'20{year}-{mounth}-10')
